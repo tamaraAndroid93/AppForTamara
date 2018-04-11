6a39ade7fc73dbd2c9bbbb6b6b9e1288e231887c
tamaraAndroid93 <tamaraAndroid93@users.noreply.github.com>
2018-04-11 16:29:41 +0200

    ContactActivity

--- app/src/main/java/com/example/djordjeblagojevic/myapplication/ContactActivity.java
+++ app/src/main/java/com/example/djordjeblagojevic/myapplication/ContactActivity.java
@@ -6,14 +6,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
-
+//fragment
 public class ContactActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private ImageView imageView; // m na nazive
 
     private TextView textViewNameDetail,textViewNumberDetail;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,47 +19,26 @@ public class ContactActivity extends AppCompatActivity {
         setContentView(R.layout.display_contact_details);
 
         incCompo();
-
         Contacts contacts;
-
-
-        contacts = (Contacts)getIntent().getSerializableExtra("position");
-
-
+        contacts = (Contacts)getIntent().getSerializableExtra("position");// position konstanta
         loadContacts(contacts);
-
-
-
-
-
-
+        
     }
 
 
     private void loadContacts (Contacts contacts){
-
+       // obican kontekst
         Picasso.with(getApplicationContext()).load(contacts.photoUri).into(imageView);
 
         if(contacts.photoUri==null){
-
             imageView.setImageResource(R.drawable.ic_launcher_background);
         }
-
+        
         textViewNameDetail.setText(contacts.name);
-
         textViewNumberDetail.setText(contacts.number);
-
         textViewNameDetail.setContentDescription(contacts.name);
-
         textViewNumberDetail.setContentDescription(contacts.number);
     }
-
-
-
-
-
-
-
 
     private void incCompo(){
 
